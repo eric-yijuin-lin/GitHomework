da81c4e148c1b49176501c8ff8759cb32a762f64
Eric Lin <eric.yijuin.lin@gmail.com>
2022-04-13 12:08:54 +0800

    在 local master 修改 5

--- GitHomework/Program.cs
+++ GitHomework/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine("在 GitHub 修改模擬別人 Push 1");
 Console.WriteLine("在 GitHub 修改模擬別人 Push 2");
 Console.WriteLine("在 local master 修改 3");
 Console.WriteLine("在 local master 修改 4");
+
+Console.WriteLine("在 local master 修改 5");
